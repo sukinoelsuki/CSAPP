a2edab60095e0368ac60435a406129a3f74002eb
sukinoelsuki <sukinoelsuki@gmail.com>
2026-04-12 18:43:45 +0800

    start the structure

--- lab1/lab1_ops.c
+++ lab1/lab1_ops.c
@@ -1,7 +1,17 @@
-#include "stdio.h"
-#include "stdlib.h"
+#include "lab1_ops.h"
 
 int pack_student_bytebybyte(student_t* s, int sno, char* buf) {
+    int cnt = 0;
+    return cnt;
+}
+
+int pack_student_whole(student_t* s, int sno, char* buf) {
+    int cnt = 0;
+    return cnt;
+
+}
+
+int restore_student(char* buf, int len, student_t* s) {
     int cnt = 0;
     return cnt;
 }
--- lab1/lab1_stu.h
+++ lab1/lab1_stu.h
@@ -2,11 +2,12 @@
 #define __lab1_student__
 
 typedef struct student {
-    char name[8];
-    short age;
-    float score;
-    char remark[200];
+    char name[8];         // 8 bytes
+    short age;            // 2 bytes
+    float score;          // 4 bytes
+    char remark[200];     // 200 bytes
 
-} student_t;
+} student_t;              // 8 + 2 + 4 + 200 = 214 bytes
+
 
 #endif
